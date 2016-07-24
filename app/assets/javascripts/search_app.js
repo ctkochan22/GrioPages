0b889a93c46f719dead85b01dfb638f515378625
ctkochan22 <ctkochan22@gmail.com>
2016-07-24 11:36:21 -0700

    Add Client-Side Javascript to handle Search Sumbission
    --File Breakdown--
    
    search_app.js:
    - Add document ready
    - Add Search Module IIFE
    - Add search for listener
    - Add method that hits find route

--- app/assets/javascripts/search_app.js
+++ app/assets/javascripts/search_app.js
@@ -1,4 +1,28 @@
 $(document).ready(function(){
   console.log("Connected");
-
+  Search.form_listener();
 })
+
+var Search = (function(){
+  return{
+    form_listener: function(){
+      $("#search_container").on("submit", "#search_form", function(event){
+        event.preventDefault();
+        var formData = $("input[name='search']").serialize();
+      })
+    },
+    request_search: function(formData){
+      var ajaxFind =
+        $.ajax({
+          type: "POST",
+          url: "search/find",
+          data: "formData",
+          dataType: "HTML"
+        });
+
+      ajaxFind.done(function(response){
+        console.log(response);
+      })
+    }
+  }
+}())
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,7 @@
 class SearchController < ApplicationController
   def index
   end
+
+  def find
+  end
 end
