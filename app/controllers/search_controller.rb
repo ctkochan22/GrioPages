cc059d36ed2356e317d6e5e295d244945cd1f477
ctkochan22 <ctkochan22@gmail.com>
2016-07-24 14:09:12 -0700

    Clean up unused code and added documentation

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,23 +3,33 @@ class SearchController < ApplicationController
   def index
   end
 
+  # FIND route takes search term and queries the database based on type of input
+  # INPUT: Expects a params with a search terms
+  # RETURN html template -- Results in a list
   def find
     search_term = params["search"]
+
     if !(search_term.match(/[a-zA-Z]/))
-      puts ("search phone numbers only")
+      #Only has digits -- Searches Phone
       clean_search_term = search_term.gsub(/[\D]/,'')
       @results = Contact.where("phone REGEXP :search", search: search_term)
+
     elsif !(search_term.match(/\d/))
-      puts ("search names only")
+      #Only has ASCII characters -- Searches Names
       @results = Contact.where("name REGEXP :search", search: search_term)
+
     else
-      puts("search line1 and line2")
+      #Searches Address
       @results = Contact.where("line1 REGEXP :search OR line2 REGEXP :search", search: search_term)
     end
 
     render :file => "/search/_results.html.erb", layout: false
   end
 
+
+  # DISPLAY route queries database with contact id
+  # INPUT Expects a params with target contact id
+  # RETURN html template -- contact with info
   def display
     contact_id = params["id"].to_i
     @contact = Contact.find_by(id: contact_id)
