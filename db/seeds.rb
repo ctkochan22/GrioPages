5e28cb878fcc8300b487d45ea6e4be3bd6b31214
ctkochan22 <ctkochan22@gmail.com>
2016-07-24 11:01:05 -0700

    Seed db with data from data.json file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+#Scrubs phone number string of any non-digit characters
+#Input: string
+#Return: string
+def clean_phone(number)
+  return number.gsub(/[\D]/,'')
+end
+
+json = ActiveSupport::JSON.decode(File.read("db/data.json"))
+
+json.each do |contact|
+  to_clean = contact
+  to_clean["phone"] = clean_phone(contact["phone"])
+  Contact.create(to_clean)
+end
